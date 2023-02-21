8bfb12761a417854380e59b843193b8b21c8cbe6
Yessly Mayorga <106281825+YessPuentes12@users.noreply.github.com>
2023-02-21 09:49:17 -0600

    Add files via upload

---
+++ EVA1_9_STACKOVERFLOW/src/eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,18 @@
+/*
+
+ */
+package eva1_9_stackoverflow;
+
+
+
+
+public class EVA1_9_STACKOVERFLOW {
+
+
+    public static void main(String[] args) {
+     A(); 
+    }
+      public static void A(){
+        A();  
+     } 
+}
